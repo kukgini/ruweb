2f79c65842c108dc41c22693f8f289cb183e4b0d
kukgini <kukgini@gmail.com>
2022-11-07 17:52:12 +0900

    running web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
+use actix_web::{middleware, web, App, HttpServer};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Opts {
+    #[clap(short, long, default_value = "9020")]
+    port: u32,
+    #[clap(short, long, default_value = "false")]
+    interactive: String,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"));
+    let opts: Opts = Opts::parse();
+    let host = std::env::var("HOST").unwrap_or("0.0.0.0".to_string());
     println!("Hello, world!");
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .wrap(middleware::NormalizePath::new(
+                middleware::TrailingSlash::Trim,
+            ))
+    })
+    .keep_alive(std::time::Duration::from_secs(30))
+    .client_request_timeout(std::time::Duration::from_secs(30))
+    .workers(1)
+    .bind(format!("{}:{}", host, opts.port))?
+    .run()
+    .await
 }
